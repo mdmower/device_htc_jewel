jrior001/twrp-htc-evita

Prepare for cm-10.2/cm-11.0
Change-Id: Ia1d156d9eb817ca62ebc4bb9d163e4cfb62364ef

BoardConfig.mk
@@ -16,20 +16,18 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := krait
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80400000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_PREBUILT_KERNEL := device/htc/evita/kernel
-
-# for jellybean branch of CM use BOARD_FORCE_RAMDISK_ADDRESS
-# for cm-10.1 branch of CM use BOARD_MKBOOTIMG_ARGS
-# BOARD_FORCE_RAMDISK_ADDRESS := 0x81800000
+TARGET_KERNEL_CONFIG := elite_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 
 # Filesystem (cat /proc/emmc)
@@ -48,6 +46,10 @@ BOARD_VOLD_MAX_PARTITIONS := 36
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 
+# Charge mode
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
+
 # TWRP
 DEVICE_RESOLUTION := 720x1280
 TW_FLASH_FROM_STORAGE := true
@@ -56,3 +58,5 @@ RECOVERY_GRAPHICS_USE_LINELENGTH := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TW_INCLUDE_DUMLOCK := true
 TW_INCLUDE_JB_CRYPTO := true
+TW_NO_SCREEN_BLANK := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"

mdmower/device_htc_jewel

BoardConfig.mk
@@ -16,20 +16,19 @@ TARGET_ARCH := arm
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := krait
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a9
 ARCH_ARM_HAVE_TLS_REGISTER := true
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80400000
 BOARD_KERNEL_PAGESIZE := 2048
-TARGET_PREBUILT_KERNEL := device/htc/jewel/kernel
 
-# for jellybean branch of CM use BOARD_FORCE_RAMDISK_ADDRESS
-# for cm-10.1 branch of CM use BOARD_MKBOOTIMG_ARGS
-# BOARD_FORCE_RAMDISK_ADDRESS := 0x81800000
+TARGET_KERNEL_CONFIG := jet_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/msm8960
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01400000
 
 # Filesystem (cat /proc/emmc)
@@ -47,6 +46,9 @@ BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_VOLD_MAX_PARTITIONS := 38
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 
+# Charge mode
+BOARD_CHARGING_MODE_BOOTING_LPM := /sys/htc_lpm/lpm_mode
+
 # TWRP
 DEVICE_RESOLUTION := 720x1280
 TW_FLASH_FROM_STORAGE := true
@@ -59,3 +61,5 @@ TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_INCLUDE_DUMLOCK := true
 TW_INCLUDE_JB_CRYPTO := true
+TW_NO_SCREEN_BLANK := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"