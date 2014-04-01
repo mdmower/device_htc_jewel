186c1d343f56a4353298d9e76653cb54375a0580
Matt Mower <mowerm@gmail.com>
2014-04-01 18:31:19 -0500

    Override product name
    Change-Id: I75b1999f068c192959578bd9d24731c28a557c6b

--- cm.mk
+++ cm.mk
@@ -5,11 +5,14 @@ PRODUCT_RELEASE_NAME := jewel
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/htc/jewel/device_jewel.mk)
+$(call inherit-product, device/htc/jewel/device.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := jewel
 PRODUCT_NAME := cm_jewel
 PRODUCT_BRAND := htc
-PRODUCT_MODEL := jewel
-PRODUCT_MANUFACTURER := htc
+PRODUCT_MODEL := EVO
+PRODUCT_MANUFACTURER := HTC
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=jewel
